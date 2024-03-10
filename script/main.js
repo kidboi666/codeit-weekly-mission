bdbedf5b5095bcc0fa02c4191fd6e091f724ed81
kidboi666 <kidboi666@gmail.com>
2024-03-10 12:48:11 +0900

    fix: main js파일 더미 코드 제거

--- script/main.js
+++ script/main.js
@@ -1,19 +1,3 @@
-const $textBox = document.querySelectorAll('.image_description');
-const $figcaption = document.querySelectorAll('.figcaption_wrap');
-const $imageBox = document.querySelectorAll('.card_image');
-
-// const handleDescription = () => {
-//   if (window.innerWidth <= 767) {
-//     for (let i = 0; i < $imageBox.length; i++) {
-//       $imageBox[i].append($textBox[i]);
-//     }
-//   } else {
-//     for (let j = 0; j < $figcaption.length; j++) {
-//       $figcaption[j].append($textBox[j]);
-//     }
-//   }
-// };
-
 const handleNavShadow = () => {
   const $nav = document.querySelector('.header_nav');
   if (window.scrollY > 30) {
@@ -23,6 +7,4 @@ const handleNavShadow = () => {
   $nav.classList.remove('nav_shadow');
 };
 
-// handleDescription();
-window.addEventListener('resize', handleDescription);
 window.addEventListener('scroll', handleNavShadow);
