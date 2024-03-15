129779e8c8fca0fbcd8db1164c04925302e10ae4
kidboi666 <kidboi666@gmail.com>
2024-03-15 15:31:17 +0900

    fix: 회원가입 페이지 패스워드 검증 함수 버그 수정

--- script/signin.js
+++ script/signin.js
@@ -6,30 +6,30 @@ import {
   $pwAlertDiv,
   $emailAlertDiv,
   validateEmailValue,
-  validatePasswordValue,
-  handlePasswordSight,
-  mockUserInformation,
+  validatePwValue,
+  handlePwSight,
+  mockUserInfo,
   drawAlert,
   message,
+  eraseAlert,
 } from './utils/auth.js';
-/* 테스트 계정과 입력값 일치 여부 검증 함수 */
+
+// 임시 계정 일치 검사 || 경고 메시지 출력
 const submitEvent = (event) => {
   const { value: emailValue } = $emailInput;
-  const { value: passwordValue } = $pwInput;
-  if (emailValue === mockUserInformation.email && passwordValue === mockUserInformation.password) {
-    location.href = '../pages/folder.html';
-  } else if (emailValue === mockUserInformation.email && passwordValue !== mockUserInformation.password) {
-    drawAlert($pwAlertDiv, $pwInput, message.wrongPassword);
-  } else if (emailValue !== mockUserInformation.email && passwordValue === mockUserInformation.password) {
-    drawAlert($emailAlertDiv, $emailInput, message.wrongEmail);
-  } else {
-    drawAlert($pwAlertDiv, $pwInput, message.wrongPassword);
-    drawAlert($emailAlertDiv, $emailInput, message.wrongEmail);
-  }
+  const { value: pwValue } = $pwInput;
   event.preventDefault();
+
+  if (emailValue === mockUserInfo.email && pwValue === mockUserInfo.pw) return (location.href = '../pages/folder.html');
+  pwValue !== mockUserInfo.pw //
+    ? drawAlert($pwAlertDiv, $pwInput, message.wrongPw)
+    : eraseAlert($pwAlertDiv, $pwInput);
+  emailValue !== mockUserInfo.email
+    ? drawAlert($emailAlertDiv, $emailInput, message.wrongEmail)
+    : eraseAlert($emailAlertDiv, $emailInput);
 };
 
 $emailInput.addEventListener('focusout', validateEmailValue);
-$pwInput.addEventListener('focusout', validatePasswordValue);
+$pwInput.addEventListener('focusout', validatePwValue);
 $form.addEventListener('submit', submitEvent);
-$eyes.addEventListener('click', handlePasswordSight);
+$eyes.addEventListener('click', handlePwSight);
--- script/signup.js
+++ script/signup.js
@@ -7,57 +7,71 @@ import {
   $pwVerifyAlertDiv,
   $form,
   $eyes,
-  mockUserInformation,
+  mockUserInfo,
   message,
   drawAlert,
   eraseAlert,
-  handlePasswordSight,
+  handlePwSight,
   validateEmailValue,
-} from './utils/auth.js'; // 패스워드 인풋창이랑 패스워드 재입력 인풋창
+  validatePwValue,
+  signupPageStatus,
+} from './utils/auth.js';
 import { numberPattern as numPattern, englishPattern as engPattern } from './utils/regx.js';
+let { duplicateAccountState, pwFormState, comparePwState } = signupPageStatus;
 
-let clickable = false;
+// 계정 중복 검사
+const checkDuplicateAccount = ({ value: emailValue }) => {
+  const validateResult = emailValue !== mockUserInfo.email;
+  validateResult === false /* 이메일이 mockUser와 같다면 */
+    ? (drawAlert($emailAlertDiv, $emailInput, message.duplicateEmail), (duplicateAccountState = false))
+    : (duplicateAccountState = true);
+};
 
-const checkDuplicateAccount = () => {
+// 비밀번호 양식 검사
+const checkPwForm = () => {
+  const { value: pwValue } = $pwInput;
+  const validateResult = engPattern.test(pwValue) && numPattern.test(pwValue) && pwValue.length >= 8;
+  validateResult === false /* 비밀번호 양식이 불일치하다면 */
+    ? (drawAlert($pwAlertDiv, $pwInput, message.wrongPwForm), (pwFormState = false))
+    : (eraseAlert($pwAlertDiv, $pwInput), (pwFormState = true));
+};
+
+// 비밀번호 재입력 일치 확인
+const comparePw = () => {
+  const { value: pwValue } = $pwInput;
+  const { value: pwVerifyValue } = $pwVerifyInput;
+  if (!pwVerifyValue) {
+    drawAlert($pwVerifyAlertDiv, $pwVerifyInput, message.wrongPw);
+  } else {
+    const validateResult = pwValue === pwVerifyValue;
+    validateResult === false /* 비밀번호와 비밀번호재입력 인풋값이 불일치하다면 */
+      ? (drawAlert($pwVerifyAlertDiv, $pwVerifyInput, message.diffrentPw), (comparePwState = false))
+      : (eraseAlert($pwVerifyAlertDiv, $pwVerifyInput), (comparePwState = true));
+  }
+};
+
+//
+const submitEvent = (e) => {
   const { value: emailValue } = $emailInput;
-  if (emailValue === mockUserInformation.email) {
-    drawAlert($emailAlertDiv, $emailInput, message.duplicateEmail);
-    clickable = false;
+  const { value: pwValue } = $pwInput;
+  const { value: pwVerifyValue } = $pwVerifyInput;
+  e.preventDefault();
+  comparePw();
+  checkPwForm();
+  checkDuplicateAccount($emailInput);
+
+  if (duplicateAccountState && pwFormState && comparePwState) {
+    return (location.href = '../pages/folder.html');
   }
+  if (emailValue === mockUserInfo.email) drawAlert($emailAlertDiv, $emailInput, message.duplicateEmail);
+  if (!emailValue) drawAlert($emailAlertDiv, $emailInput, message.wrongEmail);
+  if (!pwValue) drawAlert($pwAlertDiv, $pwInput, message.wrongPw);
+  if (!pwVerifyValue) drawAlert($pwVerifyAlertDiv, $pwVerifyInput, message.wrongPw);
 };
 
-const checkPasswordForm = (event) => {
-  const { value } = event.target;
-  const validatePasswordForm = engPattern.test(value) && numPattern.test(value) && value.length >= 8;
-  if (!validatePasswordForm) {
-    drawAlert($pwAlertDiv, $pwInput, message.wrongPasswordForm);
-    clickable = false;
-  } else {
-    eraseAlert($pwAlertDiv, $pwInput);
-  }
-};
-
-const comparePasswords = () => {
-  if (!$pwInput.value || $pwInput.value !== $pwVerifyInput.value) {
-    drawAlert($pwVerifyAlertDiv, $pwVerifyInput, message.diffrentPassword);
-    clickable = false;
-  } else {
-    eraseAlert($pwVerifyAlertDiv, $pwVerifyInput);
-    clickable = true;
-  }
-};
-
-const submitEvent = (event) => {
-  // document.querySelector('.signup_button').focus();
-  if (clickable) {
-    location.href = '../pages/folder.html';
-  }
-  event.preventDefault();
-};
-
-$eyes.addEventListener('click', handlePasswordSight);
 $emailInput.addEventListener('focusout', validateEmailValue);
-$emailInput.addEventListener('focusout', checkDuplicateAccount);
-$pwInput.addEventListener('focusout', checkPasswordForm);
-$pwVerifyInput.addEventListener('change', comparePasswords);
+$pwInput.addEventListener('focusout', validatePwValue);
+$pwInput.addEventListener('focusout', checkPwForm);
+$pwVerifyInput.addEventListener('focusout', comparePw);
 $form.addEventListener('submit', submitEvent);
+$eyes.addEventListener('click', handlePwSight);
