0c5d9e9bf6ede3f389848769f92623db31e1a35a
kidboi666 <kidboi666@gmail.com>
2024-04-28 15:06:55 +0900

    fix : 네비게이션 로직 수정

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,62 +1,42 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import * as S from './Nav.styled';
 import UserLoggedIn from './UserLoggedIn';
 import logo from '../../assets/icons/logo.svg';
 
 const Nav = () => {
-  const [isNavFixed, setIsNavFixed] = useState(true);
-  const navRef = useRef();
+  const [isShadow, setShadow] = useState(false);
   const { pathname } = useLocation();
 
-  const currentPageIsFolderPage = useCallback(() => {
-    const isFolderPage = pathname === '/folder';
-    setIsNavFixed(isFolderPage);
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
   const handleNavigation = () => {
-    if (!navRef.current) return;
-    const navNode = navRef.current;
+    setTimeout(() => {
+      if (window.scrollY > 30) {
+        setShadow(true);
+        return;
+      }
 
-    if (window.scrollY > 30) {
-      navNode.classList.add('shadow');
-      return;
-    }
-
-    navNode.classList.remove('shadow');
+      setShadow(false);
+    }, 100);
   };
 
   useEffect(() => {
+    const isFolderPage = pathname === '/folder';
+    if (isFolderPage) return;
     window.addEventListener('scroll', handleNavigation);
-    currentPageIsFolderPage();
     return () => {
       window.removeEventListener('scroll', handleNavigation);
     };
-  }, [pathname, currentPageIsFolderPage]);
+  }, [pathname]);
 
   return (
-    <>
-      {isNavFixed ? (
-        <S.HeaderLayout>
-          <S.LogoBox>
-            <Link to='/'>
-              <img src={logo} alt='Linkbrary' />
-            </Link>
-          </S.LogoBox>
-          <UserLoggedIn />
-        </S.HeaderLayout>
-      ) : (
-        <S.HeaderLayout ref={navRef}>
-          <S.LogoBox>
-            <Link to='/'>
-              <img src={logo} alt='Linkbrary' />
-            </Link>
-          </S.LogoBox>
-          <UserLoggedIn />
-        </S.HeaderLayout>
-      )}
-    </>
+    <S.HeaderLayout $isShadow={isShadow}>
+      <S.LogoBox>
+        <Link to='/'>
+          <img src={logo} alt='Linkbrary' />
+        </Link>
+      </S.LogoBox>
+      <UserLoggedIn />
+    </S.HeaderLayout>
   );
 };
 
--- src/components/Nav/Nav.styled.js
+++ src/components/Nav/Nav.styled.js
@@ -11,12 +11,14 @@ export const HeaderLayout = styled.div`
   z-index: 100;
   position: relative;
 
-  &.shadow {
-    box-shadow: 0 10px 30px rgba(188, 188, 188, 0.3);
-    position: sticky;
-    transition: box-shadow 0.3s;
-    border-bottom: 1px solid #e2ebfa;
-  }
+  ${({ $isShadow }) =>
+    $isShadow &&
+    `
+  box-shadow: 0 10px 30px rgba(188, 188, 188, 0.3);
+  position: sticky;
+  transition: box-shadow 0.3s;
+  border-bottom: 1px solid #e2ebfa;
+  `}
 
   @media (max-width: 1124px) {
     padding: 20px 32px;
