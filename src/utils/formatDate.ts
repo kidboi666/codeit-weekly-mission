89040e0bea3daafad296f074e12dccd5cdaaa73f
kidboi666 <kidboi666@gmail.com>
2024-05-06 13:45:52 +0900

    chore : next.js 초기화

--- src/utils/formatDate.ts
+++
@@ -1,6 +0,0 @@
-const formatDate = (time: string) => {
-  const date = new Date(time);
-  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
-};
-
-export default formatDate;
