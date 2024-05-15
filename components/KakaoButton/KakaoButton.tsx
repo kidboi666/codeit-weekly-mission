fd6780d9c38670185b7575fda6fbd5ad8c75e1a1
kidboi666 <kidboi666@gmail.com>
2024-05-15 14:40:08 +0900

    fix : 환경변수 수정

--- components/KakaoButton/KakaoButton.tsx
+++ components/KakaoButton/KakaoButton.tsx
@@ -11,45 +11,45 @@ const KakaoButton: React.FC = () => {
   let kakao: any;
   if (window.Kakao) {
     kakao = window.Kakao;
+  }
 
-    const realUrl = `https://codeit-weekly-mission.vercel.app`;
-    const clientKey = process.env.KAKAO_API_KEY;
+  const realUrl = `https://codeit-weekly-mission.vercel.app`;
+  const clientKey = process.env.KAKAO_API_KEY;
 
-    kakao.cleanup();
-    kakao.init(`${clientKey}`);
+  kakao.cleanup();
+  kakao.init(`${clientKey}`);
 
-    const shareMessage = () => {
-      kakao.Share.sendDefault({
-        objectType: "feed",
-        content: {
-          title: "Linkbrary",
-          description: "위클리미션",
-          imageUrl:
-            "https://5rolling.netlify.app/static/media/logo.c1f18ffcdc76df4e2e28b644ee3ae6fb.svg",
+  const shareMessage = () => {
+    kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "Linkbrary",
+        description: "위클리미션",
+        imageUrl:
+          "https://5rolling.netlify.app/static/media/logo.c1f18ffcdc76df4e2e28b644ee3ae6fb.svg",
+        link: {
+          mobileWebUrl: realUrl,
+          webUrl: realUrl,
+        },
+      },
+      buttons: [
+        {
+          title: "웹에서 확인하기",
           link: {
             mobileWebUrl: realUrl,
             webUrl: realUrl,
           },
         },
-        buttons: [
-          {
-            title: "웹에서 확인하기",
-            link: {
-              mobileWebUrl: realUrl,
-              webUrl: realUrl,
-            },
-          },
-        ],
-      });
-    };
+      ],
+    });
+  };
 
-    return (
-      <div onClick={shareMessage}>
-        <Image src={kakaoIcon} alt={kakaoIcon} />
-        <p>카카오톡</p>
-      </div>
-    );
-  }
+  return (
+    <div onClick={shareMessage}>
+      <Image src={kakaoIcon} alt={kakaoIcon} />
+      <p>카카오톡</p>
+    </div>
+  );
 };
 
 export default KakaoButton;
--- utils/copyToClipboard.ts
+++ utils/copyToClipboard.ts
@@ -1,6 +1,8 @@
 const copyToClipboard = async (folderId: number) => {
   try {
-    await navigator.clipboard.writeText(`https://codeit-weekly-mission.vercel.app/shared/${folderId}`);
+    await navigator.clipboard.writeText(
+      `https://codeit-weekly-mission.vercel.app/sharedPage/${folderId}`,
+    );
   } catch (error) {
     console.error(error);
   }
