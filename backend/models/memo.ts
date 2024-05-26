abaa0394394e2bb3c15f13ea06e413b8773ce7ed
kidboi666 <kidboi666@gmail.com>
2024-05-26 18:42:53 +0900

    docs : 미구현 사항 정리, 메모 모델에 백그라운드 추가

--- backend/models/memo.ts
+++ backend/models/memo.ts
@@ -4,6 +4,7 @@ const memoSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, default: "" },
     content: { type: String, required: true, default: "" },
+    background: { type: String, required: true, default: "var(--gray5-color)" },
   },
   {
     timestamps: true,
--- src/store/reducers/memo.ts
+++ src/store/reducers/memo.ts
@@ -13,23 +13,23 @@ const memoSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(getMemo.pending, (state, action) => {
+      .addCase(getMemo.pending, (state) => {
         state.status = API_MSG.PEN;
       })
       .addCase(getMemo.fulfilled, (state, action) => {
         state.data = action.payload;
         state.status = API_MSG.FUL;
       })
-      .addCase(getMemo.rejected, (state, action) => {
+      .addCase(getMemo.rejected, (state) => {
         state.status = API_MSG.REJ;
       })
-      .addCase(postMemo.pending, (state, action) => {
+      .addCase(postMemo.pending, (state) => {
         state.status = API_MSG.PEN;
       })
-      .addCase(postMemo.fulfilled, (state, action) => {
+      .addCase(postMemo.fulfilled, (state) => {
         state.status = API_MSG.FUL;
       })
-      .addCase(postMemo.rejected, (state, action) => {
+      .addCase(postMemo.rejected, (state) => {
         state.status = API_MSG.REJ;
       });
   },
