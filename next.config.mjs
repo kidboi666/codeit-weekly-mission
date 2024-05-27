647b9b260872522f36ae6c4374b5e71447e48d88
kidboi666 <kidboi666@gmail.com>
2024-05-27 11:02:56 +0900

    chore : 환경 변수 수정 및 next cors 허용 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,28 @@ const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com", "cloudflare-ipfs.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000"}, // prettier-ignore
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"}, // prettier-ignore
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"}, // prettier-ignore
+        ],
+      },
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://codeit-weekly-mission.vercel.app"}, // prettier-ignore
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"}, // prettier-ignore
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"}, // prettier-ignore
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
